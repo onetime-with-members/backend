f9701aea1f8ebdc5771f51d9a2f03ba64712afd3
bbbang105 <2018111366@dgu.ac.kr>
2025-11-16 16:29:44 +0900

    [chore] : 에브리타임 API 호출용 Feign 클라이언트를 추가한다

---
+++ src/main/java/side/onetime/global/config/FeignConfig.java
@@ -0,0 +1,26 @@
+package side.onetime.global.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.RequestInterceptor;
+
+@Configuration
+@EnableFeignClients(basePackages = "side.onetime.infra")
+public class FeignConfig {
+
+	/**
+	 * Everytime API용 Request Interceptor
+	 */
+	@Bean
+	public RequestInterceptor everytimeRequestInterceptor() {
+		return requestTemplate -> {
+			// "everytime"으로 시작하는 Feign Client에만 헤더 적용
+			if (requestTemplate.feignTarget().name().startsWith("everytime")) {
+				requestTemplate.header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/5.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+				requestTemplate.header("Referer", "https://everytime.kr/");
+			}
+		};
+	}
+}
