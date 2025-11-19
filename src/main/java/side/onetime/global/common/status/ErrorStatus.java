77e35a6e431c7b1fc7d9a3f2cea97eed05a5b157
bbbang105 <2018111366@dgu.ac.kr>
2025-11-20 00:40:17 +0900

    [refactor] : 전역 예외 코드들의 코드값을 수정한다

--- src/main/java/side/onetime/global/common/status/ErrorStatus.java
+++ src/main/java/side/onetime/global/common/status/ErrorStatus.java
@@ -9,15 +9,15 @@ import side.onetime.global.common.dto.ErrorReasonDto;
 @Getter
 @AllArgsConstructor
 public enum ErrorStatus implements BaseErrorCode {
-    // 전역 에러
-    _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"500", "서버 내부 오류가 발생했습니다. 자세한 사항은 백엔드 팀에 문의하세요."),
-    _BAD_REQUEST(HttpStatus.BAD_REQUEST,"400", "입력 값이 잘못된 요청 입니다."),
-    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"401", "인증이 필요 합니다."),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "403", "금지된 요청 입니다."),
-    _METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "405", "허용되지 않은 요청 메소드입니다."),
-    _UNSUPPORTED_MEDIA_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "415", "지원되지 않는 미디어 타입입니다."),
-    _NOT_FOUND_HANDLER(HttpStatus.NOT_FOUND, "404", "해당 경로에 대한 핸들러를 찾을 수 없습니다."),
-    _FAILED_TRANSLATE_SWAGGER(HttpStatus.INTERNAL_SERVER_ERROR, "500", "Rest Docs로 생성된 json파일을 통한 스웨거 변환에 실패하였습니다.")
+    // 전역 예외
+	_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E_INTERNAL_SERVER_ERROR", "서버 내부 오류가 발생했습니다. 자세한 사항은 백엔드 팀에 문의하세요."),
+	_BAD_REQUEST(HttpStatus.BAD_REQUEST, "E_BAD_REQUEST", "입력 값이 잘못된 요청 입니다."),
+	_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "E_UNAUTHORIZED", "인증이 필요 합니다."),
+	_FORBIDDEN(HttpStatus.FORBIDDEN, "E_FORBIDDEN", "금지된 요청 입니다."),
+	_METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "E_METHOD_NOT_ALLOWED", "허용되지 않은 요청 메소드입니다."),
+	_UNSUPPORTED_MEDIA_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "E_UNSUPPORTED_MEDIA_TYPE", "지원되지 않는 미디어 타입입니다."),
+	_NOT_FOUND_HANDLER(HttpStatus.NOT_FOUND, "E_NOT_FOUND_HANDLER", "해당 경로에 대한 핸들러를 찾을 수 없습니다."),
+	_FAILED_TRANSLATE_SWAGGER(HttpStatus.INTERNAL_SERVER_ERROR, "E_FAILED_TRANSLATE_SWAGGER", "Rest Docs로 생성된 json파일을 통한 스웨거 변환에 실패하였습니다.")
     ;
 
     private final HttpStatus httpStatus;
