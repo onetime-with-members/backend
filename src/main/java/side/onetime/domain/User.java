f6523d8eab4379fe956f320e13cd45bbb4be38fa
anxi01 <tjdals2893@naver.com>
2025-11-23 20:35:21 +0900

    #297 [refactor]: providerId not null 제약조건을 제거한다

--- src/main/java/side/onetime/domain/User.java
+++ src/main/java/side/onetime/domain/User.java
@@ -38,7 +38,7 @@ public class User extends BaseEntity {
     @Column(name = "provider", nullable = false, length = 50)
     private String provider;
 
-    @Column(name = "provider_id", nullable = false, length = 50, unique = true)
+    @Column(name = "provider_id", length = 50, unique = true)
     private String providerId;
 
     @Column(name = "service_policy_agreement")
