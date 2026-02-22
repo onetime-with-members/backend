8e86f614af084411ecd02e5b1a9679a75fc4eb80
SeongMin Han <123073840+anxi01@users.noreply.github.com>
2026-02-22 15:56:52 +0900

    feat: 참여한 이벤트 조회 API에 확정 시간 데이터를 추가한다 (#339)
    * refactor: (#338) ConfirmationDto를 클래스로 분리한다
    
    * feat: (#338) 참여 이벤트 조회 응답에 확정 시간 데이터를 추가한다
    
    * test: (#338) ConfirmationDto를 responseFields에 추가한다
    
    * test: (#338) 누락된 optional()을 추가한다

--- src/main/java/side/onetime/dto/event/response/GetEventResponse.java
+++ src/main/java/side/onetime/dto/event/response/GetEventResponse.java
@@ -1,16 +1,14 @@
 package side.onetime.dto.event.response;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
 import side.onetime.domain.Event;
 import side.onetime.domain.EventConfirmation;
 import side.onetime.domain.enums.Category;
 import side.onetime.domain.enums.EventStatus;
 import side.onetime.domain.enums.ParticipationRole;
+
+import java.util.List;
 
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record GetEventResponse(
@@ -24,29 +22,6 @@ public record GetEventResponse(
         ParticipationRole participationRole,
         ConfirmationDto confirmation
 ) {
-    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
-    public record ConfirmationDto(
-            String startDate,
-            String endDate,
-            String startDay,
-            String endDay,
-            String startTime,
-            String endTime,
-            LocalDateTime createdDate
-    ) {
-        public static ConfirmationDto from(EventConfirmation confirmation) {
-            return new ConfirmationDto(
-                    confirmation.getStartDate(),
-                    confirmation.getEndDate(),
-                    confirmation.getStartDay(),
-                    confirmation.getEndDay(),
-                    confirmation.getStartTime(),
-                    confirmation.getEndTime(),
-                    confirmation.getCreatedDate()
-            );
-        }
-    }
-
     public static GetEventResponse of(Event event, List<String> ranges, ParticipationRole participationRole, EventConfirmation eventConfirmation) {
         return new GetEventResponse(
                 String.valueOf(event.getEventId()),
