16ae75642067902bb4cb3a1bde4a6f4f74edb462
anxi01 <tjdals2893@naver.com>
2026-03-29 22:28:22 +0900

    refactor: (#350) 불필요한 import 를 제거한다

--- src/main/java/side/onetime/service/EmailEventListener.java
+++ src/main/java/side/onetime/service/EmailEventListener.java
@@ -1,7 +1,6 @@
 package side.onetime.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
